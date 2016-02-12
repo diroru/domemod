23d3f123d4aa63fffcd58040e8a8ecdf3431804d
Dimitar Ruszev <dimitar.ruszev@gmail.com>
2016-02-13 00:39:43 +0100

    apparently fixed issue, also erroneous graticule offset

--- glsl/src/utils/getGrid.glsl
+++ glsl/src/utils/getGrid.glsl
@@ -1,6 +1,12 @@
 #pragma glslify: rad2Deg = require('./rad2Deg.glsl')
+#pragma glslify: deg2Rad = require('./deg2Rad.glsl')
 
 const float PI = 3.14159265359;
+
+float myMod(float f, float m) {
+		float a = floor(f / m);
+		return f - f/a;
+}
 
 //TODO: gratGlobalOffset maybe?
 //
@@ -13,17 +19,27 @@ vec3 getLatitudeGrid(vec2 longLat, float gratOffset, float gratWidth, vec3 gratC
 
 vec3 getLongtitudeGrid(vec2 longLat, float gratOffset, float gratWidth, vec3 gratColour) {
 	float aa = 0.1;
-	float alpha_threshold = asin(gratOffset / gratWidth );
+	//float alpha_threshold = asin(deg2Rad(gratOffset) / deg2Rad(gratWidth));
+	//loat alpha_threshold = PI * 0.008;
 	float longDeg = rad2Deg(longLat.x);
 	float latDeg = rad2Deg(longLat.y);
-	if (longLat.y < alpha_threshold || longLat.y > (PI * 0.5 - alpha_threshold)) {
+	/*
+	if (longLat.y < alpha_threshold || longLat.y > (PI - alpha_threshold)) {
 		return gratColour;
 	} else {
+	*/
 		float go = gratWidth / sin(longLat.y);
-		float gr = mod(longDeg + go * 0.5, gratOffset) - go * 0.5;
+		float gr = mod(longDeg + go , gratOffset) - go;
 		// return mix(gratColour, vec3(0.0), smoothstep(go*0.5 - aa, go*0.5 + aa, abs(gr)));
 		return mix(gratColour, vec3(0.0), step(go, abs(gr)));
-	}
+	//}
+}
+
+vec3 getLongtitudeGridAlt(vec2 longLat, float gratOffset, float gratWidth, vec3 gratColour) {
+	float aa = 0.1;
+	float gr = mod(rad2Deg(longLat.x) + gratWidth * 0.5, gratOffset) - gratWidth * 0.5;
+	// return mix(gratColour, vec3(0.0), smoothstep(gratWidth*0.5 - aa, gratWidth*0.5 + aa, abs(gr)));
+	return mix(gratColour, vec3(0.0), step(gratWidth, abs(gr)));
 }
 
 
@@ -33,8 +49,10 @@ vec3 getGrid(vec2 longLat, vec3 colour) {
 	vec3 latGrid_0 = getLatitudeGrid(longLat, 45.0, 0.6, colour);
 	vec3 latGrid_1 = getLatitudeGrid(longLat, 15.0, 0.2, colour);
 	vec3 grid_rgb = longGrid_0 + longGrid_1 + latGrid_0 + latGrid_1;
+	//grid_rgb = longGrid_0;
 	//TODO eg. vec3(0.0) as const
 	return clamp(grid_rgb, vec3(0.0), vec3(1.0));
+	//return grid_rgb;
 }
 
 #pragma glslify: export(getGrid)
